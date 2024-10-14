eb7d56ca4b344228678013de9fd4be5fde7e96d2
Diki Faturrohman <118197085+DikiFaturrohman@users.noreply.github.com>
2024-10-14 14:14:37 +0700

    Delete kotlin-dasar directory

--- kotlin-dasar/src/DoWhileLoops.kt
+++
@@ -1,9 +0,0 @@
-fun main(){
-    // kode blok dijalankan dahulu, diakhir dilakukan pengecekan
-    var i = 0
-
-    do{
-        println("Loops = $i")
-        i++
-    } while (i <= 10)
-}
--- kotlin-dasar/src/UnitReturningFunction.kt
+++
@@ -1,8 +0,0 @@
-// function unit tidak mengembalikan data, di java disebut void
-fun printHello(): Unit{
-    println("Hello")
-}
-
-fun main(){
-    printHello()
-}
